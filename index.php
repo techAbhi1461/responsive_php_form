34cefe1eb17b37a4e77e4ff9e6db0b294d1e7db9
techAbhi1461 <magicalbhakuni@gmail.com>
2017-08-08 18:18:33 -0500

    gave name to the inputs

--- index.php
+++ index.php
@@ -1,41 +1,42 @@
+<?php include('form_validation.php'); ?>
 <link rel="stylesheet" href="form.css" type="text/css">
 <div id="container">
-<form id="contact-form" action"/" method="post">
+<form id="contact-form" action"<?php $_SERVER['PHP_SELF']; ?>" method="post">
   <h3>Send an Email:</h3>
   <h4>Fill in the form below, and I'll get back to you within 24 hours</h4>
   
   <div>
     <label>
       <span>Name: (required)</span>
-      <input placeholder="Please enter your name" type="text" tabindex="1" autofocus>
+      <input placeholder="Please enter your name" name="name" type="text" tabindex="1" autofocus>
     </label>
   </div>
   
   <div>
     <label>
       <span>Email: (required)</span>
-      <input placeholder="Please enter your email address" type="text" tabindex="2">
+      <input placeholder="Please enter your email address" name="email" type="text" tabindex="2">
     </label>
   </div>
   
   <div>
     <label>
       <span>Telephone: (required)</span>
-      <input placeholder="Please enter your number" type="text" tabindex="3">
+      <input placeholder="Please enter your number" name="phone" type="text" tabindex="3">
     </label>
   </div>
   
   <div>
     <label>
       <span>Subject: (optional)</span>
-      <input placeholder="Subject" type="text" tabindex="4">
+      <input placeholder="Subject" name="subject" type="text" tabindex="4">
     </label>
   </div>
   
   <div>
     <label>
       <span>Message: (required)</span>
-      <textarea placeholder="Include all the details you can" tabindex="5"></textarea>
+      <textarea placeholder="Include all the details you can" name="message" tabindex="5"></textarea>
     </label>
   </div>
   
