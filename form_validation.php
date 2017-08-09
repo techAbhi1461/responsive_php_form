7f0c2e5b91afd9efcf313dc5a2b76a5b69572ddc
techAbhi1461 <magicalbhakuni@gmail.com>
2017-08-09 16:54:09 -0500

    Added a check for empty required fields

--- form_validation.php
+++ form_validation.php
@@ -1,7 +1,38 @@
 <?php
 
-print_r($_POST);
-die;
+    $name_error = $email_error = $phone_error = $sub_error = $message_error = "";
+    $name = $email = $phone = $subject = $message = "";
 
-echo"Hello";
+    function test_input($data) {
+     $data = trim($data);
+     $data = stripslashes($data);
+     $data = htmlspecialchars($data);
+     return $data;
+}                                                                                                                                                                                                               
+
+    if(empty($_POST["name"]))
+    {
+        $name_error = "Name is required";
+    } 
+
+    if(empty($_POST["email"]))
+    {
+        $email_error = "Email is required";
+    } 
+
+    if(empty($_POST["phone"]))
+    {
+        $phone_error = "Phone is required";
+    } 
+
+    if(empty($_POST["subject"]))
+    {
+        $sub_error = "";
+    } 
+
+    if(empty($_POST["message"]))
+    {
+        $message_error = "";
+    } 
+
 ?>
