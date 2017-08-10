99f5e0c7e885f20fde721387d871967152fdc74d
techAbhi1461 <magicalbhakuni@gmail.com>
2017-08-09 23:45:09 -0500

    added value in all input tags

--- index.php
+++ index.php
@@ -8,40 +8,41 @@
   <div>
     <label>
       <span>Name: (required)</span>
-      <input placeholder="Please enter your name" name="name" type="text" tabindex="1" autofocus>
-      <?php $name_error ?>
+      <input placeholder="Please enter your name" name="name" type="text" tabindex="1" value="<?php $name ?>" autofocus>
+      <span class="error">*<?php echo"$name_error" ?></span>
+
     </label>
   </div>
   
   <div>
     <label>
       <span>Email: (required)</span>
-      <input placeholder="Please enter your email address" name="email" type="text" tabindex="2">
-      <span class="error"><?php $email_error ?></span>
+      <input placeholder="Please enter your email address" name="email" type="text" value="<?php $email ?>" tabindex="2">
+      <span class="error">*<?php echo $email_error ?></span>
     </label>
   </div>
   
   <div>
     <label>
       <span>Telephone: (required)</span>
-      <input placeholder="Please enter your number" name="phone" type="text" tabindex="3">
-      <span class="error"><?php $phone_error ?></span>
+      <input placeholder="Please enter your number" name="phone" type="text" value="<?php $phone ?>" tabindex="3">
+      <span class="error">*<?php echo $phone_error ?></span>
     </label>
   </div>
   
   <div>
     <label>
       <span>Subject: (optional)</span>
-      <input placeholder="Subject" name="subject" type="text" tabindex="4">
-      <span class="error"><?php $sub_error ?></span>
+      <input placeholder="Subject" name="subject" type="text" value="<?php $subject ?>" tabindex="4">
+      <span class="error"><?php echo $sub_error ?></span>
     </label>
   </div>
   
   <div>
     <label>
       <span>Message: (required)</span>
-      <textarea placeholder="Include all the details you can" name="message" tabindex="5"></textarea>
-      <span class="error"><?php $message_error ?></span>
+      <textarea placeholder="Include all the details you can" name="message" value="<?php $message ?>" tabindex="5"></textarea>
+      <span class="error"><?php echo $message_error ?></span>
     </label>
   </div>
   
